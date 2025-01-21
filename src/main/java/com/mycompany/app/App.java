5b0164e106f1fd893a83e1a6263ed2460807357e
Bagzhan Sadvakassov <bagzhan.sadvakassov@dnapayments.com>
2025-01-21 10:38:08 +0500

    lang level++

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,9 +1,6 @@
 package com.mycompany.app;
 
-public class App
-{
-public static void main( String[] args )
-{
-    System.out.println( "Hello World!" );
-}
+public class App {
+    public static void main(String[] args) {
+    }
 }
